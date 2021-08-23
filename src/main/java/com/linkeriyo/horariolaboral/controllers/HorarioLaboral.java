fb52ff6ea32b7c42792ce623d90a8f183fe8b5db
Linkeriyo <linkeriyo@gmail.com>
2021-08-23 20:13:04 +0200

    junk deleted

--- src/main/java/com/linkeriyo/horariolaboral/controllers/HorarioLaboral.java
+++ src/main/java/com/linkeriyo/horariolaboral/controllers/HorarioLaboral.java
@@ -42,27 +42,6 @@ public class HorarioLaboral {
                     .build();
             jda.awaitReady();
 
-/*
-            MessageEmbed embed = new EmbedBuilder().setDescription("1. respetacion\n"
-                    + "2. no sé consiste en respetarse\n"
-                    + "3. si te dicen que estás fuerte probablemente es mentira\n"
-                    + "4. respeten a bad bunny (porque si no os banea vaya)\n"
-                    + "5. no ligar con menores\n"
-                    + "6. mete hellcase\n"
-                    + "7. need for speed most wanted\n"
-                    + "8. no nos gustan los chetos\n"
-                    + "9. lo primero que tienen que hacer los ricos es invitar a los colegas\n"
-                    + "10. si se te ocurre algo se lo dices a <@154268434090164226>")
-                    .setFooter("gracias", "https://mui.today/__export/1583420503184/sites/mui/img/2019/12/02/bad-bunny.jpg_1899857922.jpg")
-                    .setImage("https://cutewallpaper.org/21/minecraft-wallpaper-hd/Download-minecraft-wallpaper-hd.jpg")
-                    .setAuthor("normas y consejos", "https://discordapp.com", null)
-                    .setThumbnail("https://cdn.discordapp.com/attachments/674346221569310741/792041822561370122/doro.png")
-                    .addField("che- check this out", "[@minecraftersooc](https://twitter.com/minecraftersooc)", false)
-                    .addField("invitación permanente", "[https://discord.com/invite/PJuJMJ6](https://discord.com/invite/PJuJMJ6)", false)
-                    .build();
-
-            jda.getTextChannelById("713842133232254977").sendMessage(embed).queue();
-*/
         } catch (InterruptedException | GeneralSecurityException ex) {
             Logger.getLogger(HorarioLaboral.class.getName()).log(Level.SEVERE, null, ex);
         }
